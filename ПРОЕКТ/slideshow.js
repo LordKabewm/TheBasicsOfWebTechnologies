3c07da69abd6a86407ee3f34d4d4dcd817c10540
Lord Kabewm <fedorzhatin@yandex.ru>
2023-05-22 17:40:39 +0300

    xd

---
+++ ПРОЕКТ/slideshow.js
@@ -0,0 +1,26 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+//plusSides(n) управляет переключением
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+//А эта штука контролирует, что сейчас видит пользователь
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
